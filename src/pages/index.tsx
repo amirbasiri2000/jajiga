36074ea8e060e2cd1fa72b690f8a9af0ef97e22c
amirbasiri2000 <basiriamir2000@gmail.com>
2022-12-04 23:34:36 +0330

    complete Nav bar

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Layout from '../components/Layout'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Layout title="Home">
+        Main
+      </Layout>
+    </div>
+  )
+}
